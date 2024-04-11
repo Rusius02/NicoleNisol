0f045815c9e89f64f84be24c97bee0ba4f615c1b
Rusius02 <adrien.stievenart@live.be>
2024-04-11 23:35:38 +0200

    Opacité + changement de nom

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,7 +1,7 @@
 <template>
-     <v-container>
+     <v-container style="opacity: 0.9;">
     <v-card>
-      <v-card-title>Contact Form</v-card-title>
+      <v-card-title>Formulaire de contact</v-card-title>
       <v-card-text>
         <v-form @submit.prevent="submitForm">
           <v-text-field v-model="name" label="Name"></v-text-field>
