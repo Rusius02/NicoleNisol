61075f7fae178799005bacdc67c63eb924f36bde
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2024-09-14 14:22:01 +0200

    Mise place d'un carroussel

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,27 +1,59 @@
 <!-- Accueil.vue -->
-
 <template>
-<div class="welcome-section">
-  <div class="author-thumbnail">
-    <img src="@/assets/img/Nicole.jpeg" alt="Author Thumbnail">
-  </div>
-  <div class="welcome-text">
-    <p>Chers lecteurs, chères lectrices,<br>
-    Merci de votre visite sur mon site. <br>
-    Voici quelques précisions pour y naviguer : <br>
-    L'onglet "A propos" vous décrit mon rapport avec les mots et la manière dont je suis devenue auteure. <br>
-    La rubrique "Boutique" vous permettra d'acheter mes livres. <br>
-    Si vous voulez correspondre avec moi, vous pouvez le faire via l'onglet "Contact". Soyez assurés que je vous répondrai toujours personnellement (même si cela prend un peu de temps). <br>
-    Merci de vos lectures. <br>
-    Je vous adresse toutes mes amitiés, <br>
-    Nicole Nisol</p>
-  </div>
-</div>
-  </template>
+  <v-carousel
+    height="100%"
+    progress="primary"
+    hide-delimiters
+  >
+    <!-- First Slide: Welcome Section -->
+    <v-carousel-item>
+      <v-sheet height="100%">
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="welcome-section">
+            <div class="author-thumbnail">
+              <img src="@/assets/img/Nicole.jpeg" alt="Author Thumbnail">
+            </div>
+            <div class="welcome-text">
+              <p>Chers lecteurs, chères lectrices,<br>
+              Merci de votre visite sur mon site. <br>
+              Voici quelques précisions pour y naviguer : <br>
+              L'onglet "A propos" vous décrit mon rapport avec les mots et la manière dont je suis devenue auteure. <br>
+              La rubrique "Boutique" vous permettra d'acheter mes livres. <br>
+              Si vous voulez correspondre avec moi, vous pouvez le faire via l'onglet "Contact". Soyez assurés que je vous répondrai toujours personnellement (même si cela prend un peu de temps). <br>
+              Merci de vos lectures. <br>
+              Je vous adresse toutes mes amitiés, <br>
+              Nicole Nisol</p>
+            </div>
+          </div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+
+    <!-- Other Slides -->
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet height="100%">
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
   
   <script>
   export default {
     name: 'AccueilPage',
+    data() {
+    return {
+      slides: ['Second', 'Third', 'Fourth'] // Your other slides
+    };
+  }
   };
   </script>
   
