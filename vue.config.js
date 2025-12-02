505b63f1c3eadea84695cd0d838e6f003532173a
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2025-12-02 18:54:55 +0100

    Fix build errors

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  chainWebpack: config => {
+    // Cette section est généralement gérée par vue-cli-service,
+    // mais nous la modifions en dernier recours.
+
+    // Cible les fichiers CSS
+    config.module.rule('css')
+      .oneOf('normal')
+      .use('extract-css-loader')
+      .loader(MiniCssExtractPlugin.loader)
+      .before('css-loader') 
+
+    // Ajoutez explicitement le plugin
+    config.plugin('extract-css')
+      .use(MiniCssExtractPlugin, [{
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }]);
+  }
+};
